fd03552880c618085df24de52879fb363d92a6e3
文翼 <wenzhixin2010@gmail.com>
2014-05-07 23:51:23 +0800

    Create define-const.js

---
+++ define-const.js
@@ -0,0 +1,34 @@
+function defineConst(props) {
+	// Modern browsers, IE9+
+	var obj = {},
+		hack = !Object.defineProperty || /MSIE 8.0/.test(navigator.userAgent);
+
+	// IE6 - IE7: must be a DOM object (even if it's not a real tag) attached to document
+	// IE8 (defineProperty must be a DOM object)
+	if (hack) {
+		var onPropertyChange = function (e) {
+			// temporarily remove the event so it doesn't fire again and create a loop
+			obj.detachEvent("onpropertychange", onPropertyChange);
+			obj[e.propertyName] = props[e.propertyName];
+			alert('onPropertyChange');
+			// restore the event
+			obj.attachEvent("onpropertychange", onPropertyChange);
+		};
+		
+		obj = document.createElement('fake');
+		document.body.appendChild(obj);
+	}
+	for (var prop in props) {
+		if (hack) {
+			obj[prop] = props[prop];
+		} else {
+			Object.defineProperty(obj, prop, {writable: false, value: props[prop]});
+		}
+	}
+	
+	if (hack) {
+	    obj.attachEvent("onpropertychange", onPropertyChange);
+	}
+
+	return obj;
+}
